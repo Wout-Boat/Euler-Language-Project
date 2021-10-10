358d769def1cd27af3207dba85813f4541c76595
Wout Bouckaert <woutboat@rams.colostate.edu>
2021-10-10 17:32:16 -0600

    Problem 8

---
+++ 005/Problem5.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func problem005() {
+	var num int = 3000
+	for {
+		if num%2 == 0 {
+			if num%3 == 0 {
+				if num%4 == 0 {
+					if num%5 == 0 {
+						if num%6 == 0 {
+							if num%7 == 0 {
+								if num%8 == 0 {
+									if num%9 == 0 {
+										if num%10 == 0 {
+											if num%11 == 0 {
+												if num%12 == 0 {
+													if num%13 == 0 {
+														if num%14 == 0 {
+															if num%15 == 0 {
+																if num%16 == 0 {
+																	if num%17 == 0 {
+																		if num%18 == 0 {
+																			if num%19 == 0 {
+																				if num%20 == 0 {
+																					break
+																				}
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		num += 20
+	}
+	fmt.Println(num)
+}
+
+func main() {
+	start := time.Now()
+	problem005()
+	elapsed := time.Since(start)
+	fmt.Printf("Function took %s", elapsed)
+}
