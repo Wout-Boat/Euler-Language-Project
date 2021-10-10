e4b4545bc5315a42857565d69bf7a138c36cad45
Wout Bouckaert <woutboat@rams.colostate.edu>
2021-10-10 09:28:04 -0600

    001 go complete

---
+++ 001/001.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func problem001() {
+	var sum int = 0
+	for i := 1; i < 1000; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			sum += i
+		}
+	}
+	fmt.Println(sum)
+}
+
+func main() {
+	start := time.Now()
+	problem001()
+	elapsed := time.Since(start)
+	fmt.Printf("Function took %s", elapsed)
+}
