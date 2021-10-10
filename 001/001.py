f37e71764ed9b1daa7b95120d6a76fe0dbca5a45
Wout Bouckaert <woutboat@rams.colostate.edu>
2021-10-10 09:43:36 -0600

    Python 001

---
+++ 001/001.py
@@ -0,0 +1,15 @@
+import time
+
+
+def problem001():
+    su = 0
+    for i in range(1, 1000):
+        if i % 3 == 0 or i % 5 == 0:
+            su += i
+    print(su)
+
+
+start = time.time()
+problem001()
+end = time.time()
+print("Function took " + str(end - start))
