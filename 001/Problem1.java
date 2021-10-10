767d57fb1e62b5c4e97078d1a1830475f4856340
Wout Bouckaert <woutboat@rams.colostate.edu>
2021-10-10 10:05:22 -0600

    Java Problem 1

---
+++ 001/Problem1.java
@@ -0,0 +1,18 @@
+class Problem1 {
+    public static void main(String[] args) {
+        final long startTime = System.nanoTime();
+        runProblem();
+        final long endTime = System.nanoTime();
+        System.out.println("Function took " + (endTime - startTime));
+    }
+
+    static void runProblem() {
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
