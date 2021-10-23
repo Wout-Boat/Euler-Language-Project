6cb0b0da621ae6f91f9b68a8015c7f5a8a29eaca
Wout Bouckaert <woutbouckaert@gmail.com>
2021-10-23 12:32:47 -0600

    Rust Problem 3

---
+++ 003/Problem3.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let start = std::time::SystemTime::now();
+    problem();
+    match start.elapsed() {
+        Ok(elapsed) => {
+            println!("Function took: {}", elapsed.as_nanos())
+        }
+        Err(e) => {
+            println!("Error: {:?}", e)
+        }
+    }
+}
+
+fn problem() {
+    let num = 600851475143;
+    let factors = primes::factors(num);
+
+    println!("{:?}", factors.last().unwrap());
+}
