7cbf1b941f90bf441e24f3ffe7074274fae99658
Wout Bouckaert <woutbouckaert@gmail.com>
2021-10-23 15:46:06 -0600

    Rust Problem 9

---
+++ 009/Problem9.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let start = std::time::SystemTime::now();
+    problem();
+    match start.elapsed() {
+        Ok(elapsed) => {
+            println!("Function took: {}", elapsed.as_nanos())
+        }
+        Err(e) => {
+            println!("Error: {:?}", e)
+        }
+    }
+}
+
+fn problem() {
+    let mut c: usize;
+    let s: usize = 1000;
+    let mut ans: usize = 10;
+    for a in 1..s/3 {
+        for b in a..s/2 {
+            c = s - a - b;
+
+            if a * a + b * b == c * c {
+                ans = a * b * c;
+                break;
+            }
+        }
+    }
+    println!("{}", ans);
+}
